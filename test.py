a137829e2dcbed809123d90847c781d3a82ed6c4
Akhil NS <nshyamakhil@gmail.com>
2025-02-10 18:58:31 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        numset = {}
+        freq = [[] for i in range(0,len(nums)+1)]
+        
+        for num in nums:
+            numset[num] = 1 + numset.get(num,0)
+        for n,i in numset.items():
+            freq[i].append(n)
+
+        res = []
+        for index in range(len(freq)-1,-1,-1):
+            for n in freq[index]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+
+
+        
